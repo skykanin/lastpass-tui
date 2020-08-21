b842f41304d4654612aaa1a07cd1eece32eca982
skykanin <3789764+skykanin@users.noreply.github.com>
2020-08-21 15:47:45 +0200

    Implement add and remove item commands

--- src/Main.hs
+++ src/Main.hs
@@ -12,13 +12,12 @@ module Main where
 
 import           CLI                            ( User(..)
                                                 , startSession
-                                                , addItem
                                                 , getItems
                                                 , endSession
                                                 , getUser
                                                 )
-import           Parse.Types
 import           System.Exit                    ( ExitCode(..) )
+
 main :: IO ()
 main = do
   user <- getUser
@@ -26,18 +25,6 @@ main = do
   case exit of
     ExitFailure _ -> putStrLn "Failed to login"
     ExitSuccess   -> do
-      _     <- addItem myItem
       items <- getItems (_passwd user)
       print items
       endSession
- where
-  myItem = MkLogin
-    (Login { _id       = ""
-           , _name     = "xXmemerboi420Xx"
-           , _username = "BBBBBOIIIIIIIIIII"
-           , _password = "pogupogu2"
-           , _group    = ""
-           , _url      = "https://memehub.com"
-           , _note     = "random meme account"
-           }
-    )
